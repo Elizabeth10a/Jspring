89e9aa38e584c96d27eb41f6aec29887f827b051
lubu <ygreen10.a@gmail.com>
2022-01-06 16:09:11 +0800

    1

---
+++ Sprioc/src/main/kotlin/com/eliza/sprioc/SpriocApplication.kt
@@ -0,0 +1,11 @@
+package com.eliza.sprioc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpriocApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpriocApplication>(*args)
+}
